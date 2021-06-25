68c878deea959fcb04949039c2844072aabb1901
KenZync <kenphati@hotmail.com>
2021-06-25 17:48:41 +0700

    q2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [filterItems, setFilterItems] = useState([]);
+  const [items, setItems] = useState([]);
+  const getData = () => {
+    axios
+      .get("https://api.publicapis.org/categories")
+      .then((response) => {
+        console.log(response);
+        setItems(response.data);
+        setFilterItems(response.data);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  function handleSearch(e) {
+    setFilterItems(
+      items.filter(
+        (item) => item.toLowerCase().indexOf(e.target.value.toLowerCase()) >= 0
+      )
+    );
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Filter<input type="text" onChange={handleSearch}></input>
+      <table>
+        <thead>
+          <tr>
+            <th>Categories</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filterItems.map((item) => (
+            <tr>
+              <td>{item}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
